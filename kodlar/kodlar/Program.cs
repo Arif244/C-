45e8ee188232bd71ca31bde856892ea263635293
Arif244 <117301795+Arif244@users.noreply.github.com>
2022-11-17 17:38:16 +0400

    tapsiriqlar

--- kodlar/kodlar/Program.cs
+++ kodlar/kodlar/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region eded 2 nin quvvetidirmi
+
+
+Console.WriteLine("eded 2 nin quvvetidirmi");
+//string numStr = Console.ReadLine();
+//int num = Convert.ToInt32(numStr);
+
+int num = int.Parse(Console.ReadLine());
+if(num%2==0)
+{
+    if (num / 2 == 2) ;
+    Console.WriteLine("quvvetidir");
+}
+else
+{
+    Console.WriteLine("quvveti deyil");
+}
+#endregion
+
+
+#region Verilmiş n və m (n<m) ədədləri arasında neçə tək ədəd olduğunu tapın. 
+
+Int array = { n, m };
+int count = 0;
+
+for (int i = n; i < array.length; i++)
+{
+    i % 2 == 1;
+    count++;
+    Console.WriteLine(count);
+
+}
+
+#endregion
+
+
+#region 3.Verilen eded 70-den boyukdurse,70-le arasindaki ferqin 3 qatini ekrana yazdiran,eger kicikdirse ferqini yazdiran alqoritim .
+int num1 = int.Parse(Console.ReadLine());
+int result = 1;
+
+for (int i = num; i>70; )
+{
+    if ( )
+    {
+
+    }
+}
+
+#endregion
