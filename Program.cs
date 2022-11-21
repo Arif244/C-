6d8bf755a81ef71fd1bebd239b7dddf4c26778d9
Arif244 <117301795+Arif244@users.noreply.github.com>
2022-11-21 11:51:52 +0400

    ev tapsirigi

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Schema;
+
+namespace Tapsiriq_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Total(20,25,);
+            Total(55, 30);
+            Total(32, 6);
+        }
+        static void Total();
+
+        static void Total(int number1, int number2);
+
+        Console.WriteLine("Total is" + (number1+number2));
+
+       
+            static void Total();
+
+        static void Total(int number1, int number2);
+
+        Console.WriteLine("Total is" + (number1-number2);
+
+
+            static void Total();
+
+        static void Total(int number1, int number2);
+
+        Console.WriteLine("Total in "+(number1*number2);
+    }
+}
